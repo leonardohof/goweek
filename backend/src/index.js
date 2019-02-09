33912f265b56d1c99e575caa7374b4f58a17ba22
Leonardo Miranda <leonardom@ciandt.com>
2019-02-09 15:52:47 -0200

    init

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+mongoose.connect("mongodb://localhost:27017/goweek-backend", {
+  useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes"));
+
+server.listen(9999, () => {
+  console.log("=> Server started on port 3002");
+});
